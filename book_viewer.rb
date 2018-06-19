ccf81a8cd814c24081db69488ea278ef5ffe834f
matochondrion <matochondrion@gmail.com>
2018-06-19 22:49:59 +0000

    add code for list view

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,8 +2,20 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  @title = 'The Adventures of Shirlock Holmes'
+  # @title = 'The Adventures of Shirlock Holmes'
+  # @contents = File.readlines('data/toc.txt')
+
+  # erb :home
+  @files = Dir.glob('public/*.*').map { |file| File.basename(file) }.sort
+  @files.reverse! if params[:sort] == 'desc'
+
+  erb :list
+end
+
+get '/chapters/1' do
+  @title = 'Chapter 1'
+  @chapter = File.read('data/chp1.txt')
   @contents = File.readlines('data/toc.txt')
 
-  erb :home
+  erb :chapter
 end
