c2b80c4c849070fd3276f29a7938cf64f748ed73
matochondrion <matochondrion@gmail.com>
2018-06-20 07:17:17 +0000

    add search page and functionality

--- book_viewer.rb
+++ book_viewer.rb
@@ -15,6 +15,30 @@ helpers do
   end
 end
 
+# Calls the block for each chapter, passing that chapter's number, name, and
+# # contents.
+def each_chapter
+  @contents.each.with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << {number: number, name: name} if contents.include?(query)
+  end
+
+  results
+end
+
 not_found do
   redirect ('/')
 end
@@ -49,3 +73,9 @@ end
 get '/show/:name' do
   params[:name]
 end
+
+get '/search' do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
