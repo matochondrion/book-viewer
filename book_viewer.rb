b2f8dbd14267c8f1bf0909013052ada719b9e9f4
matochondrion <matochondrion@gmail.com>
2018-06-20 04:32:12 +0000

    add layout filter and helper

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,21 +1,44 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get "/" do
-  # @title = 'The Adventures of Shirlock Holmes'
-  # @contents = File.readlines('data/toc.txt')
+before do
+  @contents = File.readlines('data/toc.txt')
+end
 
-  # erb :home
+helpers do
+  def in_paragraphs(text)
+    result = text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end
+
+    result.join("\n")
+  end
+end
+
+get "/" do
+  @title = 'The Adventures of Shirlock Holmes'
+
+  erb :home
+end
+
+get '/list' do
+  @title = 'List'
   @files = Dir.glob('public/*.*').map { |file| File.basename(file) }.sort
+
   @files.reverse! if params[:sort] == 'desc'
 
   erb :list
 end
 
-get '/chapters/1' do
-  @title = 'Chapter 1'
-  @chapter = File.read('data/chp1.txt')
-  @contents = File.readlines('data/toc.txt')
+get '/chapters/:number' do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get '/show/:name' do
+  params[:name]
 end
